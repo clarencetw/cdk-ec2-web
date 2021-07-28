216068f9924a94a104d4e0fc8902a3f29fda24c7
Clarence <me@clarence.tw>
2021-07-28 22:43:41 +0800

    Add vpc

--- lib/cdk-ec2-web-stack.ts
+++ lib/cdk-ec2-web-stack.ts
@@ -1,9 +1,10 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
 
 export class CdkEc2WebStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'VPC');
   }
 }
