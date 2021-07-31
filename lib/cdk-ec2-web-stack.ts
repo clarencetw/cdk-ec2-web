687f9de361425c6705e8542a277cae85b503a960
Clarence <me@clarence.tw>
2021-08-01 02:12:52 +0800

    Add ec2 use key pair

--- lib/cdk-ec2-web-stack.ts
+++ lib/cdk-ec2-web-stack.ts
@@ -5,6 +5,37 @@ export class CdkEc2WebStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'VPC');
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      natGateways: 1
+    });
+
+    const securityGroup = new ec2.SecurityGroup(this, "SecurityGroup", {
+      vpc,
+      description: "Allow ssh access to ec2 instances",
+      allowAllOutbound: true,
+    });
+    securityGroup.addIngressRule(
+      ec2.Peer.ipv4('114.114.192.168/32'),
+      ec2.Port.tcp(22),
+      "Allow ssh access from clarence"
+    );
+
+    const ec2Instance = new ec2.Instance(this, "Instance", {
+      vpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T3,
+        ec2.InstanceSize.NANO
+      ),
+      machineImage: new ec2.AmazonLinuxImage(),
+      securityGroup,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      keyName: "Clarence",
+    });
+
+    new cdk.CfnOutput(this, 'InstanceIP', {
+      value: ec2Instance.instancePublicDnsName
+    })
   }
 }
