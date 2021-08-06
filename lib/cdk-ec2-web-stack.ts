48f68ea7a189aa1266c84050ca8a8c53ee200912
Clarence <me@clarence.tw>
2021-08-07 02:59:35 +0800

    Add private ec2 and public ec2

--- lib/cdk-ec2-web-stack.ts
+++ lib/cdk-ec2-web-stack.ts
@@ -1,15 +1,20 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
-import * as iam from '@aws-cdk/aws-iam';
 
 export class CdkEc2WebStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'VPC', {
-      natGateways: 1
+    const vpc = ec2.Vpc.fromLookup(this, 'VPC', {
+      isDefault: true,
     });
 
+    const host = new ec2.BastionHostLinux(this, "BastionHost", {
+      vpc,
+      subnetSelection: { subnetType: ec2.SubnetType.PUBLIC },
+    });
+    host.allowSshAccessFrom(ec2.Peer.ipv4('114.114.192.168/32'));
+    
     const ec2Instance = new ec2.Instance(this, "Instance", {
       vpc,
       instanceType: ec2.InstanceType.of(
@@ -17,23 +22,12 @@ export class CdkEc2WebStack extends cdk.Stack {
         ec2.InstanceSize.NANO
       ),
       machineImage: new ec2.AmazonLinuxImage(),
+      keyName: "Clarence",
     });
-    ec2Instance.addToRolePolicy(
-      new iam.PolicyStatement({
-        actions: [
-          "ssmmessages:*",
-          "ssm:UpdateInstanceInformation",
-          "ec2messages:*",
-        ],
-        resources: ["*"],
-      })
-    );
-    ec2Instance.addUserData(
-      "yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm"
-    );
+    ec2Instance.connections.allowFrom(host, ec2.Port.tcp(22))
 
-    new cdk.CfnOutput(this, 'InstanceId', {
-      value: ec2Instance.instanceId
+    new cdk.CfnOutput(this, 'PublicDnsName', {
+      value: ec2Instance.instancePublicDnsName
     })
   }
 }
