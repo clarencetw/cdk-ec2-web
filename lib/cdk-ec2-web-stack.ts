b181b78d63e21536294662c119fa6e3d6bac544c
Clarence <me@clarence.tw>
2021-08-08 03:00:15 +0800

    Add alb to ec2

--- lib/cdk-ec2-web-stack.ts
+++ lib/cdk-ec2-web-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
+import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
+import * as targets from "@aws-cdk/aws-elasticloadbalancingv2-targets";
 import * as assets from '@aws-cdk/aws-s3-assets';
 import * as path from 'path';
 
@@ -10,8 +12,8 @@ export class CdkEc2WebStack extends cdk.Stack {
     const vpc = ec2.Vpc.fromLookup(this, 'VPC', {
       isDefault: true,
     });
+    
     const asset = new assets.Asset(this, 'Asset', { path: path.join(__dirname, '../ec2-configure/configure.sh') });
-
     const instance = new ec2.Instance(this, "Instance", {
       vpc,
       instanceType: ec2.InstanceType.of(
@@ -22,12 +24,8 @@ export class CdkEc2WebStack extends cdk.Stack {
         generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
       }),
       keyName: "Clarence",
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PUBLIC,
-      },
     });
-    instance.connections.allowFromAnyIpv4(ec2.Port.tcp(80))
-    instance.connections.allowFrom(ec2.Peer.ipv4('114.32.192.99/32'), ec2.Port.tcp(22))
+    instance.connections.allowFrom(ec2.Peer.ipv4('114.114.192.168/32'), ec2.Port.tcp(22))
 
     const localPath = instance.userData.addS3DownloadCommand({
       bucket: asset.bucket,
@@ -39,11 +37,21 @@ export class CdkEc2WebStack extends cdk.Stack {
     });
     asset.grantRead(instance.role);
 
-    new cdk.CfnOutput(this, 'PublicDnsName', {
-      value: instance.instancePublicDnsName
-    })
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
+      vpc,
+      internetFacing: true
+    });
+    const listener = lb.addListener('Listener', {
+      port: 80,
+      open: true,
+    });
+    listener.addTargets('ApplicationFleet', {
+      port: 80,
+      targets: [new targets.InstanceTarget(instance)]
+    });
+    listener.connections.allowTo(instance, ec2.Port.tcp(80));
     new cdk.CfnOutput(this, 'PHPInfo', {
-      value: `http://${instance.instancePublicDnsName}/phpinfo.php`
+      value: `http://${lb.loadBalancerDnsName}/phpinfo.php`
     })
   }
 }
